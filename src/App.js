97394fce1a9374733190ba0f12a63a248851f698
Atereoyinn <98267555+Atereoyinn@users.noreply.github.com>
2022-11-21 13:09:42 -0800

    Deleted the test file

--- src/App.js
+++ src/App.js
@@ -2,17 +2,17 @@ import { Container } from "./styles/globalStyle";
 import Sidebar from "./components/Sidebar/index.js";
 import Main from "./components/Main/index.js";
 import Trend from "./components/Trend/index.js";
-import Test from "./Test"
+
 function App() {
   return (
-    <Test />
+    <Container>
+    <Sidebar />
+    <Main />
+    <Trend />
+</Container>
   
   );
 }
 
 export default App;
-  {/*<Container>
-      <Sidebar />
-      <Main />
-      <Trend />
-  </Container>*/}
+ 
